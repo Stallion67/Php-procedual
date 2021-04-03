cd769e568f57c801d393245aa318e0a9099aafdb
Ben Okello Mwaka <mwakaben@gmail.com>
2021-04-04 00:15:16 +0300

    Select ,Update, Delete and setData  Order  in SQL

---
+++ SQL/Select From Posts table.sql
@@ -0,0 +1,6 @@
+select * from posts;  /*Selects all from database*/
+
+select * from posts where id ='1';  /*Selects all from postswhere subject is 1*/
+
+select subject from posts;  /*Selects all subjects from posts Table*/
+
---
+++ SQL/Set Data Order form Mysql DB.sql
@@ -0,0 +1,9 @@
+SELECT * FROM posts ORDER BY id ASC /*odrer by Ascending */
+
+/*Oder by Deceinding*/
+SELECT * FROM posts ORDER BY id DESC
+
+/* Odrer by Subject Asscending */
+SELECT * FROM posts ORDER BY subject ASC
+
+/*Group By */
