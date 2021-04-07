24a1ff7007331d73c0e758bdf6f9c89321e318eb
Ben Okello Mwaka <mwakaben@gmail.com>
2021-04-08 00:48:36 +0300

    Signup and Login working with erroe Messages functioning

--- LoginSystem/signup.php
+++ LoginSystem/signup.php
@@ -6,6 +6,43 @@
         <div  class="wrapper-main">
             <section class="section-default">
                 <h1>Signup</h1>
+
+                <?php
+                
+                    if(isset($_GET['error'])){
+
+                        $SignupCheck =$_GET['error'];
+                        if($SignupCheck == "emptyfields"){
+                            echo'<p class="signuperror">Fill in all Fields !!!</p>';
+                        }
+                        else if($SignupCheck =="invalidmail&uid"){
+                            echo'<p class="signuperror">Invalid Email and User ID !!!</p>';
+                        }
+                        else if($SignupCheck =="invalidmail"){
+                            echo'<p class="signuperror">Enter a Valid E - mail address !!!</p>';
+                        }
+                        else if($SignupCheck =="invaliduid"){
+                            echo'<p class="signuperror">Invalid User ID !!!</p>';
+                        }
+                       
+                        else if($SignupCheck =="passwordcheck"){
+                            echo'<p class="signuperror">Passwords Do not match!!!</p>';
+                        }
+                        else if($SignupCheck =="sqlerror"){
+                            echo'<p class="signuperror">SQL Error!! Refresh OR Contact Support/Development IF ERROR PERSISTS </p>';
+                        }
+                        else if($SignupCheck =="usertaken"){
+                            echo'<p class="signuperror">User Name Taken new user name</p>';
+                        }
+                     
+                   
+                    } 
+                    else if($_GET['signup']=="success"){
+                        echo'<p class="signupsuccess">Succesfully Signed Up</p>';
+                       
+                    }
+                
+                ?>
                
                 <form class="form-signup" action="includes/signup.inc.php" method="post">
                        
