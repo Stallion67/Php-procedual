365dbafe09b64b3faa82fb1df6a0a2f6734cb205
Ben Okello Mwaka <mwakaben@gmail.com>
2021-06-28 00:42:09 +0300

    Modifying the User ID

--- GenerateRandomKey/index.php
+++ GenerateRandomKey/index.php
@@ -41,7 +41,7 @@
 
         echo" <br/> <br/>
         <br/>
-        <h3> Random Unique ID</h3> ";
+        <h3> Random Unique ID based</h3> ";
     
         function generateUniqueID(){      
             $randUID= uniqid(); //using the function Unique ID
@@ -51,7 +51,8 @@
 
         echo" <br/> <br/>
         <br/>
-        <h3> Random Cusrom Unique ID</h3> ";
+        <h3> Random Custom Unique ID</h3> 
+        <h4>BAsed on the Time Stamp</h4>";
     
         function generateUniqueID2(){      
             $randUID= uniqid('UID'); //using the function Unique ID
@@ -60,8 +61,57 @@
         echo generateUniqueID2();
         echo" <br/>";
         echo generateUniqueID2();
+        echo" <br/>";
+
+        echo"   <h4>Not based on timestamp but customized</h4>";
         
-        
+        function generateUniqueID3(){      
+            $randUID= uniqid('UID',true); //using the function Unique ID
+            return $randUID;
+        }
+        echo generateUniqueID3();
+
+        echo" <br/>";
+        echo" <h4>The Best Way to create a KEY</h4>";
+////////////////-----BEgin BEST KEY----///////////////////////////////
+            //DB Connection
+        $conn =mysqli_connect("localhost","root","","rims");
+
+        function checkKeys( $conn, $randStr){
+                $sql="SELECT * from users";
+                $result =mysqli_query($conn, $sql);
+
+                while($row = mysqli_fetch_assoc($result)){
+                        if($row['user_id']==$randStr){
+                            //If it exist
+                            $keyExists=true;
+                            break;
+                        } else{
+                            $keyExists=false;
+                        }
+                }
+                return $keyExists; //so that it can be used in generate UId Function
+
+        }
+
+        function generateUniqueUID($conn){      
+              $keyLength=15; //length
+              $str="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIFJLMNOPQRSTUVWXYZi"; //charater allowed
+              $randStr = substr('UiD'.str_shuffle($str),0,$keyLength); // this will return only 8 characters starting at
+             
+
+              $checkKeys = checkKeys( $conn, $randStr);
+                //Checking if check key returned ne
+              while( $checkKeys==true ){
+                $randStr = substr('UiD'.str_shuffle($str),0,$keyLength);
+                $checkKeys=checkKeys( $conn, $randStr);
+              }
+
+              return $randStr;
+
+        }
+        echo "<h3> The Unique Custom Key is : ".generateUniqueUID($conn)."</h3>";
+////////////////-----END BEST KEY----///////////////////////////////
 
     ?>
 
